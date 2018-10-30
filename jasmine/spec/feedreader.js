bdb345adde48206e4e57b83269d23a0b8183068e
Vikrant Sharma <vikkrant.xx7@gmail.com>
2018-10-30 20:30:05 +0530

    Add feed name test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name is defined', function(){
+            for(let feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
     });
 
 
